f92e35dbd5e5a930fec9718f19dbf6b1679a1cb8
Tankasala Naga Siddhardha <nagasiddhardhatankasala2008@gmail.com>
2026-08-18 13:39:42 +0530

    Add files via upload

---
+++ Week-3/lab-3(c).py
@@ -0,0 +1,18 @@
+def selection_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        min_index = i
+
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    return arr
+
+
+arr = [72, 25, 25, 22, 3]
+
+print(selection_sort(arr))
